d35ac7df731f84da332472d45fc3ed25389de9a6
mohammedari <mohammedarixx@gmail.com>
2019-09-25 01:09:06 +0900

    changed to use discard operation

--- opengl_ros_lib/shader/fs_depth_image_projection.glsl
+++ opengl_ros_lib/shader/fs_depth_image_projection.glsl
@@ -6,8 +6,9 @@ out float fragmentColor;
 
 void main(void)
 {
-    if (-gridMapLayerHeight / 2 < gl_FragCoord.z && gl_FragCoord.z < gridMapLayerHeight / 2)
-        fragmentColor = gridMapAccumulationWeight;
-    else
-        fragmentColor = 0;
+    //If the height is out of the range, discard the fragment.
+    if (gl_FragCoord.z < -gridMapLayerHeight / 2 || gridMapLayerHeight / 2 < gl_FragCoord.z)
+        discard;
+
+    fragmentColor = gridMapAccumulationWeight;
 }
